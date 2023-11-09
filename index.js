2264a77216934f6376d7effd690d61a888eed31d
Tuhin Hossain <mmtuhinhossain@gmail.com>
2023-11-09 16:45:27 +0600

    Token recreated

--- index.js
+++ index.js
@@ -49,6 +49,17 @@ async function run() {
       .collection("applied_jobs");
 
     //Auth Type API
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
+      res.cookie('token', token, {
+        httpOnly:true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 60 * 60 * 1000
+      }).send({success : true})
+    })
 
     //Create Token and send cookie to browser
 
